05835351bb974d99c162f81d6eaf75478bf30265
misu <szijjartonagy.misu@gmail.com>
2023-05-21 22:32:42 +0200

    initial

---
+++ barrack_app_django/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for barrack_app_django project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barrack_app_django.settings')
+
+application = get_asgi_application()
